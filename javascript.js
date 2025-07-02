e38e61a28b92dfbe1efdb0d55dfe3484c2423ffd
Quaza31 <ognjenristic123@gmail.com>
2025-07-02 14:27:36 +0200

    Added links for github and linkedin

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const line = this.querySelectorAll(".line");
   const overlayButtons = this.querySelector(".overlay-buttons");
   const linkMenu = this.querySelectorAll(".link-menu");
+  const linkedin = this.querySelector(".linkedin");
+  const github = this.querySelector(".github");
   menu.addEventListener("click", function () {
     overlay.classList.toggle("active");
     menu.classList.toggle("active");
@@ -15,5 +17,13 @@ document.addEventListener("DOMContentLoaded", function () {
     linkMenu.forEach(function (r) {
       r.classList.toggle("active");
     });
+  });
+
+  linkedin.addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/ognjen-ristic-0b9988347/");
+  });
+
+  github.addEventListener("click", function () {
+    window.open("https://github.com/Quaza31");
   });
 });
