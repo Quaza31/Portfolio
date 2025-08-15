11d46dc3a65e4c090ad06ab918dede303cd5efb8
Quaza31 <ognjenristic123@gmail.com>
2025-08-15 15:13:37 +0200

    Almost done

--- javascript.js
+++ javascript.js
@@ -6,6 +6,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const linkMenu = this.querySelectorAll(".link-menu");
   const linkedin = this.querySelector(".linkedin");
   const github = this.querySelector(".github");
+  const scrollArrow = this.querySelector(".scroll-arrow");
+  const VD_logo = this.querySelector(".logo");
+
+  const homeButton = this.querySelector(".home-button");
+  const home = this.querySelector(".home");
+  const aboutButton = this.querySelector(".about-button");
+  const about = this.querySelector(".about");
+  const projectsButton = this.querySelector(".projects-button");
+  const projects = this.querySelector(".projects");
+  const contactButton = this.querySelector(".contact-button")
+  const contact = this.querySelector(".contact");
+
+  
+
   menu.addEventListener("click", function () {
     overlay.classList.toggle("active");
     menu.classList.toggle("active");
@@ -17,7 +31,46 @@ document.addEventListener("DOMContentLoaded", function () {
     linkMenu.forEach(function (r) {
       r.classList.toggle("active");
     });
+  
+     if (overlay.classList.contains('active')) {
+      document.body.style.overflow = 'hidden';
+     } else {
+      document.body.style.overflow = '';
+     }
+
   });
+
+  overlayButtons.addEventListener("click", function() {
+    overlay.classList.toggle("active");
+    menu.classList.toggle("active");
+    overlayButtons.classList.toggle("active");
+    line.forEach(function (l) {
+      l.classList.toggle("active");
+    });
+
+    linkMenu.forEach(function (r) {
+      r.classList.toggle("active");
+    });
+    document.body.style.overflow = '';
+  });
+
+  homeButton.addEventListener("click", function() {
+    home.scrollIntoView({behavior: "smooth"});
+  });
+
+  aboutButton.addEventListener("click", function() {
+    about.scrollIntoView({behavior: "smooth"});
+  });
+
+  projectsButton.addEventListener("click", function() {
+    projects.scrollIntoView({behavior: "smooth"});
+  });
+
+  contactButton.addEventListener("click", function() {
+    contact.scrollIntoView({behavior: "smooth"});
+  });
+
+
 
   linkedin.addEventListener("click", function () {
     window.open("https://www.linkedin.com/in/ognjen-ristic-0b9988347/");
@@ -26,4 +79,19 @@ document.addEventListener("DOMContentLoaded", function () {
   github.addEventListener("click", function () {
     window.open("https://github.com/Quaza31");
   });
+
+  window.addEventListener("scroll", function() {
+    if(window.scrollY > 0) {
+      scrollArrow.classList.add("scrolled")
+
+    } else {
+      scrollArrow.classList.remove("scrolled")
+    }
+
+  })
+
+  VD_logo.addEventListener("click", function () {
+    window.open("https://quaza31.itch.io/vampire-defenders");
+  });
+
 });
